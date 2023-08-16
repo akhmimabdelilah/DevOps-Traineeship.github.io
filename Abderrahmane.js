9ae93da31b09ff79436ff704ed55198704f13271
ibtihal <ibtihal.abouhafc@um5r.ac.ma>
2023-08-16 15:40:23 +0100

    first commit for Abderrahmane

---
+++ Abderrahmane.js
@@ -0,0 +1,5 @@
+// Generate a random number between 1 and 100
+const randomNum = Math.floor(Math.random() * 100) + 1;
+
+// Log the random number to the console
+console.log(`Random Number: ${randomNum}`);
